3ffb580a2eebcf14cf1edceaae2ba308342cca8f
aimadnet <contact@aimadnet.com>
2023-05-04 20:31:15 +0100

    twttrapi python wrapper for rapidapi

---
+++ README.md
@@ -0,0 +1,74 @@
+# TwttrAPI - Unofficial Twitter API
+
+A Python API wrapper for TwttrAPI, an unofficial Twitter API available on RapidAPI, which is built using Twitter's mobile API.
+
+https://rapidapi.com/contact-cmWXEDTql/api/twttrapi
+
+## Installation
+
+Copy the `twttrapi.py` file into your project directory.
+
+## Usage
+
+Import the `TwttrAPIClient` class and instantiate it with your RapidAPI key:
+
+```python
+from twttrapi import TwttrAPIClient
+
+api_key = "your_rapidapi_key_here"
+client = TwttrAPIClient(api_key)
+```
+
+## Methods
+
+Here is a list of methods available in the `TwttrAPIClient` class:
+
+* `get_tweet(tweet_id)`
+* `get_tweet_conversation(tweet_id)`
+* `user_tweets(username=None, user_id=None, cursor=None)`
+* `user_media(username=None, user_id=None, cursor=None)`
+* `user_likes(username=None, user_id=None, cursor=None)`
+* `for_you_timeline(cursor=None)`
+* `following_timeline(cursor=None)`
+* `create_tweet(tweet_text, attachment_url=None, in_reply_to_tweet_id=None, media_id=None)`
+* `delete_tweet(tweet_id)`
+* `get_user(username=None)`
+* `user_followers(username=None, user_id=None, cursor=None)`
+* `user_following(username=None, user_id=None, cursor=None)`
+* `search_suggestions(query, cursor=None)`
+* `search_top(query, cursor=None)`
+* `search_latest(query, cursor=None)`
+* `search_users(query, cursor=None)`
+* `search_images(query, cursor=None)`
+* `search_videos(query, cursor=None)`
+* `login_email_username(username_or_email, password)`
+* `login_2fa(login_data, response)`
+* `logout()`
+* `follow_user(username=None, user_id=None)`
+* `unfollow_user(username=None, user_id=None)`
+* `favorite_tweet(tweet_id)`
+* `unfavorite_tweet(tweet_id)`
+* `retweet_tweet(tweet_id)`
+* `unretweet_tweet(tweet_id)`
+* `get_dm_conversations(cursor=None)`
+* `get_dm_conversation(username=None, user_id=None, cursor=None)`
+* `send_dm(message, to_user_name=None, to_user_id=None, media_id=None)`
+* `upload_image(image_url)`
+
+For more information on each method and its parameters, please refer to the `twttrapi.py` file.
+
+## Example
+
+Here's an example on how to use the API wrapper:
+
+```python
+from twttr_api_client import TwttrAPIClient
+
+api_key = "your_rapidapi_key_here"
+client = TwttrAPIClient(api_key)
+
+# Get a tweet by its ID
+tweet_id = "1652849795336159233"
+tweet = client.get_tweet(tweet_id)
+print(tweet)
+```
---
+++ twttrapi.py
@@ -0,0 +1,268 @@
+import requests
+
+class TwttrAPIClient:
+    def __init__(self, api_key, session="", proxy=""):
+        self.api_key = api_key
+        self.base_url = "https://twttrapi.p.rapidapi.com/"
+        self.twttr_session = session
+        self.twttr_proxy = proxy
+
+    def _get_headers(self):
+        return {
+            "twttr-session": self.twttr_session,
+            "twttr-proxy": self.twttr_proxy,
+            "x-rapidapi-host": "twttrapi.p.rapidapi.com",
+            "x-rapidapi-key": self.api_key,
+            "content-type": "application/json",
+        }
+
+    def _send_request(self, method, endpoint, params=None):
+        url = self.base_url + endpoint
+        headers = self._get_headers()
+
+        response = requests.request(method, url, headers=headers, params=params)
+        response.raise_for_status()
+
+        return response.json()
+    
+    def get_tweet(self, tweet_id):
+        endpoint = "get-tweet"
+        params = {
+            "tweet_id": tweet_id
+        }
+        return self._send_request("GET", endpoint, params)
+
+    def get_tweet_conversation(self, tweet_id):
+        endpoint = "get-tweet-conversation"
+        params = {
+            "tweet_id": tweet_id
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def user_tweets(self, username=None, user_id=None, cursor=None):
+        endpoint = "user-tweets"
+        params = {
+            "username": username,
+            "user_id": user_id,
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def user_media(self, username=None, user_id=None, cursor=None):
+        endpoint = "user-media"
+        params = {
+            "username": username,
+            "user_id": user_id,
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def user_likes(self, username=None, user_id=None, cursor=None):
+        endpoint = "user-likes"
+        params = {
+            "username": username,
+            "user_id": user_id,
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def for_you_timeline(self, cursor=None):
+        endpoint = "for-you-timeline"
+        params = {
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def following_timeline(self, cursor=None):
+        endpoint = "following-timeline"
+        params = {
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def create_tweet(self, tweet_text, attachment_url=None, in_reply_to_tweet_id=None, media_id=None):
+        endpoint = "create-tweet"
+        params = {
+            "tweet_text": tweet_text,
+            "attachment_url": attachment_url,
+            "in_reply_to_tweet_id": in_reply_to_tweet_id,
+            "media_id": media_id
+        }
+        return self._send_request("POST", endpoint, params)
+    
+    def delete_tweet(self, tweet_id):
+        endpoint = "delete-tweet"
+        params = {
+            "tweet_id": tweet_id
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def get_user(self, username=None):
+        endpoint = "get-user"
+        params = {
+            "username": username
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def user_followers(self, username=None, user_id=None, cursor=None):
+        endpoint = "user-followers"
+        params = {
+            "username": username,
+            "user_id": user_id,
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def user_following(self, username=None, user_id=None, cursor=None):
+        endpoint = "user-following"
+        params = {
+            "username": username,
+            "user_id": user_id,
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def search_suggestions(self, query, cursor=None):
+        endpoint = "search-suggestions"
+        params = {
+            "query": query,
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def search_top(self, query, cursor=None):
+        endpoint = "search-top"
+        params = {
+            "query": query,
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def search_latest(self, query, cursor=None):
+        endpoint = "search-latest"
+        params = {
+            "query": query,
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def search_users(self, query, cursor=None):
+        endpoint = "search-users"
+        params = {
+            "query": query,
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def search_images(self, query, cursor=None):
+        endpoint = "search-images"
+        params = {
+            "query": query,
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def search_videos(self, query, cursor=None):
+        endpoint = "search-videos"
+        params = {
+            "query": query,
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def login_email_username(self, username_or_email, password):
+        endpoint = "login-email-username"
+        params = {
+            "username_or_email": username_or_email,
+            "password": password
+        }
+        return self._send_request("POST", endpoint, params)
+    
+    def login_2fa(self, login_data, response):
+        endpoint = "login-2fa"
+        params = {
+            "login_data": login_data,
+            "response": response
+        }
+        return self._send_request("POST", endpoint, params)
+    
+    def logout(self):
+        endpoint = "logout"
+        return self._send_request("POST", endpoint)
+    
+    def follow_user(self, username=None, user_id=None):
+        endpoint = "follow-user"
+        params = {
+            "username": username,
+            "user_id": user_id
+        }
+        return self._send_request("POST", endpoint, params)
+    
+    def unfollow_user(self, username=None, user_id=None):
+        endpoint = "unfollow-user"
+        params = {
+            "username": username,
+            "user_id": user_id
+        }
+        return self._send_request("POST", endpoint, params)
+    
+    def favorite_tweet(self, tweet_id):
+        endpoint = "favorite-tweet"
+        params = {
+            "tweet_id": tweet_id
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def unfavorite_tweet(self, tweet_id):
+        endpoint = "unfavorite-tweet"
+        params = {
+            "tweet_id": tweet_id
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def retweet_tweet(self, tweet_id):
+        endpoint = "retweet-tweet"
+        params = {
+            "tweet_id": tweet_id
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def unretweet_tweet(self, tweet_id):
+        endpoint = "unretweet-tweet"
+        params = {
+            "tweet_id": tweet_id
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def get_dm_conversations(self, cursor=None):
+        endpoint = "get-dm-conversations"
+        params = {
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def get_dm_conversation(self, username=None, user_id=None, cursor=None):
+        endpoint = "get-dm-conversation"
+        params = {
+            "username": username,
+            "user_id": user_id,
+            "cursor": cursor
+        }
+        return self._send_request("GET", endpoint, params)
+    
+    def send_dm(self, message, to_user_name=None, to_user_id=None, media_id=None):
+        endpoint = "send-dm"
+        params = {
+            "message": message,
+            "to_user_name": to_user_name,
+            "to_user_id": to_user_id,
+            "media_id": media_id
+        }
+        return self._send_request("POST", endpoint, params)
+    
+    def upload_image(self, image_url):
+        endpoint = "upload-image"
+        params = {
+            "image_url": image_url
+        }
+        return self._send_request("POST", endpoint, params)
